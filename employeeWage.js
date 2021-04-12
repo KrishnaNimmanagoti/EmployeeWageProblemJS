7cfb6cfc80e970fdb188eaa973fe120c73140a14
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-12 14:24:28 +0530

    Refactored code to write function for daily working Hours

--- employeeWage.js
+++ employeeWage.js
@@ -1,22 +1,19 @@
-const IS_PRESENT_FULL_TIME = 2;
-const IS_PRESENT_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const IS_PART_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = 0;
-
-switch (empCheck) {
-    case IS_PRESENT_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    case IS_PRESENT_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-        break;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-let empWage = empHrs * WAGE_PER_HOUR;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empWage = getWorkingHours(empCheck) * WAGE_PER_HOUR;
 console.log("Daily Employee Wage : " + empWage);
