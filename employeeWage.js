82d5f8777a02870449f167b2e2fb6f84cdd8bee1
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-18 20:07:48 +0530

    Added Array to store daily wage along Total wage

--- employeeWage.js
+++ employeeWage.js
@@ -6,6 +6,10 @@ const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 10;
 const MAX_HRS_IN_MONTH = 100;
 
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArray = new Array();
+
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -17,14 +21,19 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10 % 3);
-    totalEmpHrs += getWorkingHours(empCheck);
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(calculateDailyWage(empHrs));
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total Days: " + totalWorkingDays + "\nTotal Hrs: " + totalEmpHrs + "\nEmp Wage: " + empWage);
+function calculateDailyWage(empHrs) {
+    let result = empHrs * WAGE_PER_HOUR;
+    return result;
+}
+
+console.log(empDailyWageArray);
+let empWage = calculateDailyWage(totalEmpHrs);
+console.log("Total Working Days: " + totalWorkingDays + "\nTotal Hrs: " + totalEmpHrs + "\nEmp Wage: " + empWage);
